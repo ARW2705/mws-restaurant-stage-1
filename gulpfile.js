7dc33f875bb78113077ab349e5ef160b344f39ef
Andrew Wanex <awanex2705@gmail.com>
2018-05-14 18:11:39 -0700

    feat: resize images into sm/md/lg and minify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const imageResize = require('gulp-image-resize');
+const imagemin = require('gulp-imagemin');
 const clean = require('gulp-clean');
 
 const paths = {
@@ -14,23 +15,43 @@ const paths = {
 
 gulp.task('clean-images', () => {
   console.log('Cleaning old files');
-  return gulp.src('img/sizes/**/*', {read: false})
+  return gulp.src(paths.images.dest, {read: false})
     .pipe(clean());
 });
 
 // TODO: prevent gulp from adding uneccessary folders
-gulp.task('minifyImages', ['clean-images'], () => {
-  console.log('Running gulp-imagemin');
-  gulp.src(paths.images.src)
+gulp.task('resizeImages', ['clean-images'], () => {
+  console.log('Running gulp-image-resize');
+  const imagesToResize = gulp.src(paths.images.src);
+  imagesToResize
+    .pipe(imageResize({
+      percentage: 100,
+      imageMagick: true
+    }))
+    .pipe(rename(path => path.basename = 'lg-' + path.basename))
+    .pipe(imagemin({progressive: true}))
+    .pipe(gulp.dest(paths.images.dest));
+
+  imagesToResize
     .pipe(imageResize({
       percentage: 60,
       imageMagick: true
     }))
+    .pipe(rename(path => path.basename = 'md-' + path.basename))
+    .pipe(imagemin({progressive: true}))
+    .pipe(gulp.dest(paths.images.dest));
+
+  imagesToResize
+    .pipe(imageResize({
+      percentage: 45,
+      imageMagick: true
+    }))
     .pipe(rename(path => path.basename = 'sm-' + path.basename))
+    .pipe(imagemin({progressive: true}))
     .pipe(gulp.dest(paths.images.dest));
 });
 
-const gulpTaskList = ['minifyImages'];
+const gulpTaskList = ['resizeImages'];
 
 gulp.task('default', gulpTaskList, () => {
   console.log('Gulp tasks completed');
