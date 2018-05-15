e3bbefca5a6a55edc4d67c15bab55742c8dd7848
Andrew Wanex <awanex2705@gmail.com>
2018-05-14 18:25:12 -0700

    docs: add docstring to resgister-sw.js

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,3 +1,7 @@
+/**
+ * If browser is compatible with service workers, register new service worker
+ */
+
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('sw.js')
     .then(registration => {
