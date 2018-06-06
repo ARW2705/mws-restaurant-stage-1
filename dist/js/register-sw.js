01d6f2863d33d27841deedfca10846c2bbe8ad43
Andrew Wanex <awanex2705@gmail.com>
2018-06-06 10:54:48 -0700

    chore: updated dist contents from gulp

--- dist/js/register-sw.js
+++ dist/js/register-sw.js
@@ -1,12 +1,1 @@
-'use strict';
-
-/**
- * If browser is compatible with service workers, register new service worker
- */
-
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('sw.js').then(function (registration) {
-    if (!navigator.serviceWorker.controller) return;
-    console.log('service worker registered!');
-  });
-}
+"use strict";navigator.serviceWorker&&navigator.serviceWorker.register("sw.js").then(function(r){navigator.serviceWorker.controller&&console.log("service worker registered!")});
